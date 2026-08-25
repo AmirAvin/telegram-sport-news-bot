388bc9a4bcd614c9508dce6adaa80030a3dce50b
AmirAvin <elham.jafarii64@gmail.com>
2026-08-26 01:30:32 +0330

    Create test_custom_emoji.py

---
+++ test_custom_emoji.py
@@ -0,0 +1,32 @@
+import os
+import asyncio
+from telegram import Bot
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHANNEL_ID = os.environ["CHANNEL_ID"]
+
+CUSTOM_EMOJI_ID = "5791832221211959289"
+
+
+async def main():
+
+    bot = Bot(token=BOT_TOKEN)
+
+    text = (
+        f'<tg-emoji emoji-id="{CUSTOM_EMOJI_ID}">🏆</tg-emoji> '
+        'تست لوگوی لیگ‌برتر @ligebartar24'
+    )
+
+    message = await bot.send_message(
+        chat_id=CHANNEL_ID,
+        text=text,
+        parse_mode="HTML"
+    )
+
+    print("MESSAGE SENT")
+    print("Message ID:", message.message_id)
+
+    await bot.shutdown()
+
+
+asyncio.run(main())
