d34b2dfeb404567c4ef948dc0d5193a490b346bc
AmirAvin <elham.jafarii64@gmail.com>
2026-08-25 20:18:54 +0330

    Update bot_new.py

--- bot_new.py
+++ bot_new.py
@@ -1,1 +1,19 @@
-print("Bot started")
+import os
+import asyncio
+from telegram import Bot
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+
+async def main():
+    bot = Bot(BOT_TOKEN)
+
+    updates = await bot.get_updates()
+
+    print("=== UPDATES ===")
+
+    for update in updates:
+        print(update)
+
+    await bot.shutdown()
+
+asyncio.run(main())
