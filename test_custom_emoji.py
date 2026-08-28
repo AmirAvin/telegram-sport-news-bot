0ce084b78ebd7392758c31819d66dd39372c94a9
AmirAvin <elham.jafarii64@gmail.com>
2026-08-28 15:46:49 +0330

    Update test_custom_emoji.py

--- test_custom_emoji.py
+++ test_custom_emoji.py
@@ -1,48 +1,30 @@
 import os
 import asyncio
 from telegram import Bot
-from telegram.constants import MessageEntityType
-from telegram import MessageEntity
-
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHANNEL_ID = os.environ["CHANNEL_ID"]
 
-CUSTOM_EMOJI_ID = "5791832221211959289"
+CUSTOM_EMOJI_ID = "5220181540222291016"
 
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
 
-    emoji = "🏆"
-    text = emoji + " تست دقیق Custom Emoji"
-
-    # Telegram uses UTF-16 code units for entity offsets/lengths.
-    offset = 0
-    length = len(emoji.encode("utf-16-le")) // 2
-
-    print("Emoji UTF-16 length:", length)
-
-    entities = [
-        MessageEntity(
-            type=MessageEntityType.CUSTOM_EMOJI,
-            offset=offset,
-            length=length,
-            custom_emoji_id=CUSTOM_EMOJI_ID,
-        )
-    ]
+    text = (
+        f'<tg-emoji emoji-id="{CUSTOM_EMOJI_ID}">🌪</tg-emoji> '
+        'تست ایموجی متحرک لیگ‌برتر @ligebartar24'
+    )
 
     message = await bot.send_message(
         chat_id=CHANNEL_ID,
         text=text,
-        entities=entities,
+        parse_mode="HTML"
     )
 
     print("MESSAGE SENT")
     print("Message ID:", message.message_id)
-    print("Message entities:", message.entities)
-
-    await bot.shutdown()
 
 
-asyncio.run(main())
+if __name__ == "__main__":
+    asyncio.run(main())
