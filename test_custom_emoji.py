842c9ea66dafce5d9bd9e6fa4159e3ff35e3f466
AmirAvin <elham.jafarii64@gmail.com>
2026-09-06 13:26:13 +0330

    Update test_custom_emoji.py

--- test_custom_emoji.py
+++ test_custom_emoji.py
@@ -5,16 +5,25 @@ from telegram import Bot
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHANNEL_ID = os.environ["CHANNEL_ID"]
 
-CUSTOM_EMOJI_ID = CUSTOM_EMOJI_ID = "5791645007882495057"
+CUSTOM_EMOJI_ID = "5791645007882495057"
 
 
 async def main():
+    print("START TEST")
+    print("Connecting to Telegram...")
+
     bot = Bot(token=BOT_TOKEN)
 
+    me = await bot.get_me()
+    print("BOT OK")
+    print("Bot username:", me.username)
+
     text = (
-        f'<tg-emoji emoji-id="{CUSTOM_EMOJI_ID}">🌪</tg-emoji> '
-        'تست ایموجی متحرک لیگ‌برتر @ligebartar24'
+        f'<tg-emoji emoji-id="{CUSTOM_EMOJI_ID}">⚽️</tg-emoji> '
+        'تست ایموجی لیگ‌برتر @ligebartar24'
     )
+
+    print("Sending message...")
 
     message = await bot.send_message(
         chat_id=CHANNEL_ID,
@@ -24,6 +33,9 @@ async def main():
 
     print("MESSAGE SENT")
     print("Message ID:", message.message_id)
+
+    await bot.close()
+    print("TEST FINISHED")
 
 
 if __name__ == "__main__":
